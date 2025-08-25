c93e458dfda8bca1f33afd5cf1b2e4a15505749d
Wolveryan <152795206+Wolveryan@users.noreply.github.com>
2025-08-25 14:39:08 +0530

    changes due to errors in build for sitl

--- libraries/AP_BoardConfig/AP_BoardConfig.cpp
+++ libraries/AP_BoardConfig/AP_BoardConfig.cpp
@@ -439,6 +439,15 @@ void AP_BoardConfig::init()
 
     board_setup();
 
+    AP_Proximity *prx = AP::proximity();
+    RangeFinder *rnd = AP::rangefinder();
+    if(prx->get_type(0) == AP_Proximity::Type::IRadar || rnd->get_rnd() ==RangeFinder::RangeFinder_Type::RangeFinder_TYPE_IOTECH_Radar)
+        {
+            IR24_Backend *_ir24_backend = AP::__backend();
+            _ir24_backend->ir24_init_thread();
+        }
+
+
 #if AP_RTC_ENABLED
     AP::rtc().set_utc_usec(hal.util->get_hw_rtc(), AP_RTC::SOURCE_HW);
 #endif
